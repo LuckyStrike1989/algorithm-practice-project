2a1e68d2f759aeb5ed95f18f268f44d2b9a82ba6
LuckyStrike1989 <maestrois@naver.com>
2024-08-27 09:38:25 +0900

    백준/8958/OX퀴즈

--- src/com/backjoon/coding04/Main.java
+++ src/com/backjoon/coding04/Main.java
@@ -1,5 +1,35 @@
 package com.backjoon.coding04;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			String s = br.readLine().toUpperCase();
+			
+			char[] values = s.toCharArray();
+			
+			int score = 0;
+			int result = 0;
+			for(char value : values) {				
+				if( 'O' == value) {
+					score++;
+				} else {
+					score = 0;
+				}
+				result += score;
+			}
+			
+			sb.append(result + "\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
