9b000f5cf8d5de60ccd578640e6c224d5addaf49
LuckyStrike1989 <maestrois@naver.com>
2024-08-29 16:18:22 +0900

    백준/10809/알파벳 찾기/진행중

---
+++ src/com/backjoon/coding10/Main.java
@@ -0,0 +1,28 @@
+package com.backjoon.coding10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		char[] values = s.toCharArray();
+		
+		Map<Integer, Character> map = new HashMap<Integer, Character>();
+		
+		for(char value : values) {
+			
+		}
+		
+	}
+}
