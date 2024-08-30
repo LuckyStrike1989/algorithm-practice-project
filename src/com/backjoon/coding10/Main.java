12a44d1d403ce8e7159f924570cdb1567553cba7
LuckyStrike1989 <maestrois@naver.com>
2024-08-30 09:50:19 +0900

    백준/10809/알파벳 찾기

--- src/com/backjoon/coding10/Main.java
+++ src/com/backjoon/coding10/Main.java
@@ -3,12 +3,6 @@ package com.backjoon.coding10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -16,13 +10,8 @@ public class Main {
 		
 		String s = br.readLine();
 		
-		char[] values = s.toCharArray();
-		
-		Map<Integer, Character> map = new HashMap<Integer, Character>();
-		
-		for(char value : values) {
-			
+		for(char c = 'a'; c<='z'; c++) {
+			System.out.print(s.indexOf(String.valueOf(c)) + " ");
 		}
-		
 	}
 }
