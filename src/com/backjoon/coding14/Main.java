e77d16368695578d4c5a75451cfe42f9730de7ea
LuckyStrike1989 <maestrois@naver.com>
2024-09-09 19:05:03 +0900

    15552 푸는중

---
+++ src/com/backjoon/coding14/Main.java
@@ -0,0 +1,24 @@
+package com.backjoon.coding14;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = null;
+		
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+		}
+	}
+}
