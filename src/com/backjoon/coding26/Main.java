6a3412420a8d0c469494fcc3b6dfd7151d8e15a8
LuckyStrike1989 <maestrois@naver.com>
2024-10-19 12:26:01 +0900

    백준/11650/좌표 정렬하기

---
+++ src/com/backjoon/coding26/Main.java
@@ -0,0 +1,44 @@
+package com.backjoon.coding26;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] numbers = new int[N][2];
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			numbers[i][0] = Integer.parseInt(st.nextToken());
+			numbers[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numbers, (o1, o2) -> {
+			if( o1[0] == o2[0] ) {
+				return o1[1] - o2[1];
+			} else {
+				return o1[0] - o2[0];
+			}
+		});
+		
+		for(int i=0; i<N; i++) {
+			bw.write(String.valueOf(numbers[i][0]) +" "+ String.valueOf(numbers[i][1]));
+			bw.newLine();
+		}
+		
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+}
