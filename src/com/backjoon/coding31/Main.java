b24a8206767119b2ce2bc0616d2fc469e7b44475
LuckyStrike1989 <maestrois@naver.com>
2024-12-20 10:23:34 +0900

    백준/11399/ATM

---
+++ src/com/backjoon/coding31/Main.java
@@ -0,0 +1,38 @@
+package com.backjoon.coding31;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] numbers = new int[N];
+		// int[] result = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i<numbers.length; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(numbers);
+		
+		int sum = 0;
+		int result = 0;
+		for(int i = 0; i<numbers.length; i++) {
+			sum += numbers[i];
+			// result[i] = sum;
+			result += sum;
+		}
+		
+		// System.out.println(Arrays.stream(result).sum());
+		
+		System.out.println(result);
+		
+		br.close();
+	}
+}
