1b24d25d713ebf552f8e46bc9d960f96bbe73ce0
LuckyStrike1989 <maestrois@naver.com>
2024-12-23 10:14:27 +0900

    백준/1920/수 찾기

--- src/com/backjoon/coding32/Main.java
+++ src/com/backjoon/coding32/Main.java
@@ -3,6 +3,50 @@ package com.backjoon.coding32;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		
+		int N1 = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		for(int i = 0; i < N1; i++ ) {
+			int num = Integer.parseInt(st.nextToken());
+			map.put(num, map.getOrDefault(num, 0)+1);
+		}
+			
+		
+		int N2 = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		
+		for(int i = 0; i<N2; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			int status = map.getOrDefault(num, 0);
+			
+			sb.append(status==0 ? 0 : 1).append('\n');
+		}
+		
+		System.out.println(sb);
+		
+		br.close();
+	}
+}
+
+/**
+ 이진탐색이요 하여 풀기 But 계속 시간초과뜸!! 왜지?? 이유는 한번 찾아봐야 될꺼같음!!
+ import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -24,8 +68,6 @@ public class Main {
 		int N2 = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		
-		// map 에 담아서 풀면 되지 않음?
-		
 		for(int i = 0 ; i < N2; i++) {
 			if( binarySearch(A, Integer.parseInt(st.nextToken())) ) {
 				sb.append("1\n");
@@ -35,7 +77,6 @@ public class Main {
 		}
 		
 		System.out.println(sb.toString());
-		
 		br.close();
 	}
 	
@@ -54,7 +95,7 @@ public class Main {
 				low = low + 1;
 			}
 		}
-		
 		return false;
 	}
 }
+ */
