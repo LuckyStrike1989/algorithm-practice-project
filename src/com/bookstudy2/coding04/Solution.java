109aa73aacc9c5fbe755dbef1e66b14679e50175
= <maestrois@naver.com>
2024-12-30 23:25:58 +0900

    프로그래머스/모의고사/진행중

---
+++ src/com/bookstudy2/coding04/Solution.java
@@ -0,0 +1,21 @@
+package com.bookstudy2.coding04;
+
+public class Solution {
+	public static int[] solution(int[] answers) {
+		// 수포자들의 패턴
+		int[][] pattern = {
+			{1, 2, 3, 4, 5},
+			{2, 1, 2, 3, 2, 4, 2, 5},
+			{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+		};
+		
+		// 수포자들의 점수를 저장할 배열
+		int[] scores = new int[3];
+		
+		return new int[3];
+	}
+	
+	public static void main(String[] args) {
+		int[] answers = {1,2,3,4,5};
+	}
+}
