e0f0f27948fced80d8a282b63a73c9bfde63c468
LuckyStrike1989 <maestrois@naver.com>
2025-01-06 18:20:47 +0900

    algorithm study/완전탐색/모의고사

--- src/com/bookstudy2/coding04/Solution.java
+++ src/com/bookstudy2/coding04/Solution.java
@@ -1,21 +1,45 @@
 package com.bookstudy2.coding04;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Solution {
 	public static int[] solution(int[] answers) {
-		// 수포자들의 패턴
 		int[][] pattern = {
 			{1, 2, 3, 4, 5},
 			{2, 1, 2, 3, 2, 4, 2, 5},
 			{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
 		};
 		
-		// 수포자들의 점수를 저장할 배열
 		int[] scores = new int[3];
 		
-		return new int[3];
+		int count = 0;
+		for(int i = 0; i<pattern.length; i++) {
+			for(int j = 0; j<pattern[i].length; j++) {
+				int index = j % answers.length;
+				
+				if( pattern[i][j] == answers[index] ) {
+					count++;
+				}
+			}
+			scores[i] = count;
+			count = 0;
+		}
+		
+		int maxScore = Arrays.stream(scores).max().getAsInt();
+		
+		ArrayList<Integer> answer = new ArrayList<>();
+		for(int i = 0; i < scores.length; i++) {
+			if(scores[i] == maxScore) {
+				answer.add(i+1);
+			}
+		}
+		
+		return answer.stream().mapToInt(Integer::intValue).toArray();
 	}
 	
 	public static void main(String[] args) {
 		int[] answers = {1,2,3,4,5};
+		System.out.println(Arrays.toString(solution(answers)));
 	}
 }
