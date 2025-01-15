ea86331a4b40ff33a21ef259458c2ccfd56d5ed6
= <maestrois@naver.com>
2025-01-15 23:20:56 +0900

    algorithm study / 행렬의 곱셈

--- src/com/bookstudy2/coding05/Solution.java
+++ src/com/bookstudy2/coding05/Solution.java
@@ -3,19 +3,25 @@ package com.bookstudy2.coding05;
 import java.util.Arrays;
 
 public class Solution {
+	/**
+	 * Çà·ÄÀÇ °ö¼À
+	 * @param arr1
+	 * @param arr2
+	 * @return
+	 */
 	public static int[][] solution(int[][] arr1, int[][] arr2) {
-		int x_arr1 = arr1[0].length;
-		int y_arr1 = arr1.length;
-		int x_arr2 = arr2[0].length;
-		int y_arr2 = arr2.length;
+		int row_arr1 = arr1.length;
+		int col_arr1 = arr1[0].length;
+		int row_arr2 = arr2.length;
+		int col_arr2 = arr2[0].length;
 		
-		int[][] result = new int[y_arr1][x_arr2];
+		int[][] result = new int[row_arr1][col_arr2];
 		
-		for(int i = 0; i<y_arr1; i++) {
-			for(int j = 0; j<x_arr2; j++) {
-				// result[i][j] += (arr1[i][j] * arr2[j][i]);
-				// System.out.println(arr1[i][j] * arr2[j][i]);
-				System.out.println(i + "/" + j);
+		for(int i = 0; i < row_arr1; i++) {
+			for(int j = 0; j<col_arr1; j++) {
+				for(int k = 0; k<row_arr2; k++) {
+					result[i][j] += (arr1[i][k] * arr2[k][j]);
+				}
 			}
 		}
 		
@@ -24,16 +30,17 @@ public class Solution {
 	
 	public static void main(String[] args) {
 		int[][] a1 = {
-			{1,4},
-			{3,2},
-			{4,1}
+			{2,3,2},
+			{4,2,4},
+			{3,1,4}
 		};
 		
 		int[][] a2 = {
-			{3,3},
-			{3,3}
+			{5,4,3},
+			{2,4,1},
+			{3,1,1}
 		};
 		
-		System.out.println(Arrays.toString(solution(a1, a2)));
+		System.out.println(Arrays.deepToString(solution(a1, a2)));
 	}
 }
